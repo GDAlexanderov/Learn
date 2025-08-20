98a1b2092e8a768c1f821cfa7bbc7ee64aa46b68
GDAlexanderov <G.D.Alexanderov@urfu.me>
2025-08-20 14:19:04 +0500

    new

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         String[] prefix = {"flower","flow","flowight"};
         System.out.println(longestCommonPrefix(prefix));
+        System.out.println("Перемога");
     }
 }
 
